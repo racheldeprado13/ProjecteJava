8f1875d9c3750e7e94133e18eff90876fa3cb825
alumne <alumne@alumne-PC>
2018-03-20 10:01:59 +0100

    creacio de projecte

---
+++ src/projectejava/genere.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projectejava;
+
+/**
+ *
+ * @author alumne
+ */
+public class genere {
+    
+    public enum Genere{HOME};
+        
+    
+//        public static void genereEnum(String Home, String Dona){
+//        //cas especial
+//        if(Home==null) return null;
+//        
+    }
+
